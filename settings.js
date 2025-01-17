78fae67f4d403e961f9c58476556882dc9bf2cc8
sodiq363 <ayomidevictor396@gmail.com>
2025-01-17 13:55:44 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "2347057344983"
+global.pairingNumber = "2348129405212"
 
 
 
@@ -19,17 +19,17 @@ global.anticall = false
 global.autoReadChat = false
 global.alwaysonline = true
 global.autoswview = false
-global.public = true
+global.public = false
 global.autoreact = false
 global.antidelete = false
-global.autotyping = false
+global.autotyping = true
 global.autoBio = true
 global.antiSpam = true
 global.sign = "𓃵"
 // ☞ ➻ ➽ ☛ 〆 
 global.bank = "Opay"
 global.bankname = "BLUE"
-global.accnumber = "7041039367"
+global.accnumber = "8051810884"
 global.language = "en"
 global.sessionName = "session"
 global.pairingCode = true 
